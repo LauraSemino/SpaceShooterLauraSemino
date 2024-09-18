d54bc2cfbac31400d7332a5e3c037e1204df3214
LauraSemino <150286546+LauraSemino@users.noreply.github.com>
2024-09-18 12:30:52 -0400

    Exercise 2 Complete

--- .vs/SpaceShooter/SpaceShooterLaura/Assets/Scripts/Controllers/Player.cs
+++ .vs/SpaceShooter/SpaceShooterLaura/Assets/Scripts/Controllers/Player.cs
@@ -8,34 +8,105 @@ public class Player : MonoBehaviour
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public Transform bombsTransform;
-    private float velocity = 0.01f;
-    private Vector3 Xdirection = Vector3.right
-    private Vector3 Ydirection = Vector3.up
+    private Vector3 Xvelocity = Vector3.right * 0.01f;
+    private Vector3 Yvelocity = Vector3.up * 0.01f;
 
+    public Vector3 velocity = Vector3.zero;
+    public float acceleration = 2f;
+    public float timeToReachSpeed = 0.5f;
+    public float targetSpeed = 5f;
+    public float friction = 0.02f;
+   
+    private void Start()
+    {
+        acceleration = targetSpeed / timeToReachSpeed;
+    }
     void Update()
     {
 
-       // transform.position += velocity;
+        // transform.position += velocity;
+         if (velocity.x >= targetSpeed)
+         {
+             velocity.x = targetSpeed;
+         }
+         if (velocity.x <= -targetSpeed)
+         {
+             velocity.x = -targetSpeed;
+         }
+         if (velocity.y >= targetSpeed)
+         {
+             velocity.y = targetSpeed;
+         }
+         if (velocity.y <= -targetSpeed)
+         {
+             velocity.y = -targetSpeed;
+         }
+
+        ApplyFriction();
         playerMovement();
     }
     public void playerMovement()
     {
         
-        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x >= -18)
-        { 
-            transform.position -= Xvelocity;
-        }
-        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x <= 18)
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position += Xvelocity;
+            velocity -= acceleration * transform.right * Time.deltaTime;
+            
         }
-        if (Input.GetKey(KeyCode.UpArrow) && transform.position.y <= 9)
+        if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position += Yvelocity;
+            velocity += acceleration * transform.right * Time.deltaTime;
+           
         }
-        if (Input.GetKey(KeyCode.DownArrow) && transform.position.y >= -9)
+        if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.position -= Yvelocity;
+            velocity += acceleration * transform.up * Time.deltaTime;
+            
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            velocity -= acceleration * transform.up * Time.deltaTime;
+            
+        }
+        transform.position += velocity * Time.deltaTime;
+
+
+       
+    }
+    public void ApplyFriction()
+    {
+        if (velocity.y > 0)
+        {
+            velocity.y -= friction * Time.deltaTime;
+        }
+        if (velocity.x > 0)
+        {
+            velocity.x -= friction * Time.deltaTime;
+        }
+        if (velocity.y < 0)
+        {
+            velocity.y += friction * Time.deltaTime;
+        }
+        if (velocity.x < 0)
+        {
+            velocity.x += friction * Time.deltaTime;
+        }
+
+        if (transform.position.y <= -9)
+        {
+            velocity.y = -(1.1f * velocity.y);
+        }
+        if (transform.position.y >= 9)
+        {
+            velocity.y = -(1.1f * velocity.y);
+        }
+        if (transform.position.x <= -18)
+        {
+            velocity.x = -(1.1f* velocity.x);
+        }
+        if (transform.position.x >= 18)
+        {
+            velocity.x = -(1.1f* + velocity.x);
         }
     }
 }
