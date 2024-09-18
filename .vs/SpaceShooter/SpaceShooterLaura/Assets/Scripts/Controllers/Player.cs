7d66e0b3c0e61c374431d07e96925d7c67f6618b
LauraSemino <150286546+LauraSemino@users.noreply.github.com>
2024-09-18 11:04:53 -0400

    Exercise Player Controller Complete

--- .vs/SpaceShooter/SpaceShooterLaura/Assets/Scripts/Controllers/Player.cs
+++ .vs/SpaceShooter/SpaceShooterLaura/Assets/Scripts/Controllers/Player.cs
@@ -8,10 +8,34 @@ public class Player : MonoBehaviour
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public Transform bombsTransform;
+    private float velocity = 0.01f;
+    private Vector3 Xdirection = Vector3.right
+    private Vector3 Ydirection = Vector3.up
 
     void Update()
     {
 
+       // transform.position += velocity;
+        playerMovement();
     }
-
+    public void playerMovement()
+    {
+        
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x >= -18)
+        { 
+            transform.position -= Xvelocity;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x <= 18)
+        {
+            transform.position += Xvelocity;
+        }
+        if (Input.GetKey(KeyCode.UpArrow) && transform.position.y <= 9)
+        {
+            transform.position += Yvelocity;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) && transform.position.y >= -9)
+        {
+            transform.position -= Yvelocity;
+        }
+    }
 }
