edddfce7607429bf68d95332d5567d9001cb2fa5
LauraSemino <150286546+LauraSemino@users.noreply.github.com>
2024-09-18 13:34:46 -0400

    Task 3 time limit reached. pausing point

--- .vs/SpaceShooter/SpaceShooterLaura/Assets/Scripts/Controllers/Asteroid.cs
+++ .vs/SpaceShooter/SpaceShooterLaura/Assets/Scripts/Controllers/Asteroid.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
-    public float moveSpeed;
-    public float arrivalDistance;
-    public float maxFloatDistance;
+    public float moveSpeed = 0.5f;
+    public float arrivalDistance = 0.5f;
+    public float maxFloatDistance = 1f;
+    public Vector3 direction;
+    public Vector3 velocity = Vector3.zero;
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        direction = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(transform.position.x <= direction.x + arrivalDistance && transform.position.x >= direction.x - arrivalDistance && transform.position.y <= direction.y + arrivalDistance && transform.position.y >= direction.y - arrivalDistance)
+        {
+            direction = new Vector3(transform.position.x + Random.Range(-1, maxFloatDistance), transform.position.x + Random.Range(-1, maxFloatDistance));
+        }
+        velocity += moveSpeed * direction * Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;
     }
 }
