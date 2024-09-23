f65ea7eb0533c9071f398d5597f122d82a78e11e
LauraSemino <150286546+LauraSemino@users.noreply.github.com>
2024-09-23 15:23:35 -0400

    Assignment Done

--- .vs/SpaceShooter/SpaceShooterLaura/Assets/Scripts/Controllers/Stars.cs
+++ .vs/SpaceShooter/SpaceShooterLaura/Assets/Scripts/Controllers/Stars.cs
@@ -10,5 +10,19 @@ public class Stars : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        StartCoroutine(DrawStars());
     }
+
+    IEnumerator DrawStars()
+    {
+        for (int i = 0; i < starTransforms.Capacity - 1; i++)
+        {
+            yield return new WaitForSeconds(drawingTime);
+            
+            Debug.DrawLine(starTransforms[i].position, starTransforms[i + 1].position, Color.yellow);
+            
+        }
+    }
+
+    
 }
